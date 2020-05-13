3104527459fa89c8b44ac399d6dc84d1be980b44
Stphn <stephanie1117@hotmail.es>
2020-05-13 04:57:09 -0600

    primeras

---
+++ 2.6/char_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+char str_a[7];
+str_a[0]='H';
+str_a[1]='e';
+str_a[2]='l';
+str_a[3]='l';
+str_a[4]='o';
+str_a[5]='\n';
+str_a[6]=0;
+printf(str_a);
+}
---
+++ 2.6/char_array2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{char str_a[20];
+strcpy(str_a, "hello, world¿\n");
+printf(str_a);
+
+}
